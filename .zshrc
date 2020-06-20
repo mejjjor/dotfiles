a004a46bcef44b43ca4a2b1b3ba9480a4027eba1
erik <erik.aouizerate@gmail.com>
2020-06-20 13:31:45 +0200

    init: finished

---
+++ .zshrc
@@ -0,0 +1,55 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+
+export ZSH="/home/riko/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=(git zsh-syntax-highlighting fzf)
+
+source $ZSH/oh-my-zsh.sh
+autoload -U compinit && compinit 
+
+# General
+alias d="docker"
+alias dc="docker-compose"
+
+# Git
+alias g="git"
+alias ga="git add"
+alias gaa="git add --all"
+alias gb="git branch"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gco="git checkout"
+alias gf="git fetch"
+alias glog="git log"
+alias glg="git lg"
+alias gl="git pull"
+alias gp="git push"
+alias gpf="git push --force-with-lease"
+alias gst="git status"
+alias gsu="git stash -u"
+alias gd="git diff"
+
+# ls
+alias l="ls"
+alias ls="ls --color"
+
+# ssh-agent
+alias ssha="ssh-add $HOME/.ssh/id_rsa"
+
+
+DEFAULT_USER=`whoami`
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- install.sh
+++ install.sh
@@ -70,6 +70,8 @@ cp -as $BASEDIR/.config ~/
 
 safelink $BASEDIR/.gitconfig $HOME/.gitconfig
 safelink $BASEDIR/.gitignore $HOME/.gitignore
+safelink $BASEDIR/.zshrc $HOME/.zshrc
+safelink $BASEDIR/.p10k.zsh $HOME/.p10k.zsh
 
 
 # -- [[ Package / plugins installation ]] --------------------------------------
@@ -79,13 +81,59 @@ echo -n "Do you want to check packages? ([y]es/[N]o) "
 read answer
 case $answer in
   "yes"|"y")
+    safeinstall curl
+
+    safeinstall i3xrocks-battery
+    safeinstall i3xrocks-memory
+    safeinstall i3xrocks-wifi
+    safeinstall i3xrocks-temp
+
+    safeinstall htop
+    safeinstall xcwd
+
     safeinstall vim
+    safeinstall zsh
     sudo snap install sublime-text --classic
+    sudo apt install nomachine
     sudo apt install gnome-shell-extensions-gpaste gpaste
+    sudo apt install fzf
     
+    # vscode
+    sudo apt install software-properties-common apt-transport-https wget
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+    sudo apt install code
+    
+    # regolight
+    sudo add-apt-repository ppa:regolith-linux/release
+    sudo apt install regolith-desktop i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time
+
+    # nvm
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    # node / npm by nvm 
+    nvm install node
+    npm i -g yarn
+
+    # oh-my-zsh
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+
+    # oh-my-zsh plugins
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+    # google-chrome
+    sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install google-chrome-stable
+
     echo "$(tput setaf 2)All dependencies are up to date$(tput sgr0)"
     ;;
   *)
     echo "$(tput setaf 3)Packages update skipped$(tput sgr0)"
     ;;
 esac
+
+
+sudo chsh -s /bin/zsh
